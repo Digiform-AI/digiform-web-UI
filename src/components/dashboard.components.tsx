bad50981ffd1f005b22066c14209204a001cd6b7
Dan-Yee <dyee3435@gmail.com>
2023-02-17 13:29:55 -0500

    create component for drag and drop file upload using react-drag-drop-files

---
+++ src/components/dashboard.components.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+/**
+ * Drag and Drop component for uploading files. Also supports just clicking anywhere in the area.
+ */
+export const FileDragNDrop = () => {
+    const fileUploadTypes = ["PDF"];
+
+    const [file, setFile] = useState<File | null>(null);
+
+    // this function can be changed to actually upload the file to the DB as needed
+    const onFileUpload = (file: File | null) => { setFile(file) };
+
+    return (
+        <div className = "DragNDrop">
+            <FileUploader
+                multiple = {false}
+                handleChange = {onFileUpload}
+                types = {fileUploadTypes}
+            />
+            
+            {/*Test line to make sure the file uploads correctly*/}
+            <p>{file ? `File Name: ${file.name} | File Size: ${file.size}` : "No File Uploaded Yet"}</p>
+        </div>
+    );
+}
